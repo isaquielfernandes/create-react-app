8729481d5df36a6f3d2a114aa2bf106986d7927b
IF <iisaquielfernandes@gmail.com>
2020-07-21 13:27:11 +0100

    Update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,7 +5,7 @@ const SearchForm = ({params, onParamChange}) => (
     <Form className="mb-4">
         <Form.Row className="align-items-end">
             <Form.Group as={Col}>
-                <Form.Label>Description</Form.Label>
+                <Form.Label>Job Description</Form.Label>
                 <Form.Control onChange={onParamChange} value={params.description} name="description" type="text"/>
             </Form.Group>
             <Form.Group as={Col}>
